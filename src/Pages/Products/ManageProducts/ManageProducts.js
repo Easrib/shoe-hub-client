2535c71c05c0c1ed6941b9261fd769a302bbea5e
Easrib <easrib.sharik01@gmail.com>
2022-05-10 10:31:35 +0600

    delete inventory api

--- src/Pages/Products/ManageProducts/ManageProducts.js
+++ src/Pages/Products/ManageProducts/ManageProducts.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Row } from 'react-bootstrap';
+import { Card, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import Inventory from '../Inventory/Inventory';
 
 const ManageProducts = () => {
     const [products, setProducts] = useState([]);
@@ -12,19 +11,44 @@ const ManageProducts = () => {
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+    const handleDeleteProduct = id => {
+        const proceed = window.confirm('Are you confirm? ')
+        if (proceed) {
+            const url = `http://localhost:5000/products/${id}`
+
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(result => {
+                    console.log(result);
+                    const remaining = products.filter(product=>product._id!==id);
+                    setProducts(remaining)
+                })
+
+        }
+    }
     return (
         <div>
             <h2>All Products at Warehouse</h2>
             <div className='text-center my-2'>
                 <Link to='/addproducts'><button className='btn btn-primary'>Add Products</button></Link>
             </div>
-            <Row xs={1} md={2} lg={3} className="g-4">
+            <Row xs={1} md={2} lg={3}>
                 {
-                    products.map(product => <Inventory
-                        key={product._id}
-                        product={product}
-                    >
-                    </Inventory>
+                    products.map(product =>
+                        <Card>
+                            <Card.Img variant="top" src={product.img} />
+                            <Card.Body>
+                                <Card.Title>Name : {product.name}</Card.Title>
+                                <Card.Text>
+                                    <p>Price:{product.price} </p>
+                                    <p>Quantity:{product.quantity} </p>
+                                    <p>Supplier Name: {product.seller} </p>
+                                </Card.Text>
+                                <button onClick={() => handleDeleteProduct(product._id)} className='btn btn-primary'>Delete Stock</button>
+                            </Card.Body>
+                        </Card>
                     )
                 }
 
