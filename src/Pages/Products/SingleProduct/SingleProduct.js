c057cb21aa72f4f6686862514964f7f7c29a09ce
Easrib <easrib.sharik01@gmail.com>
2022-05-12 12:42:34 +0600

    stock updated

--- src/Pages/Products/SingleProduct/SingleProduct.js
+++ src/Pages/Products/SingleProduct/SingleProduct.js
@@ -6,12 +6,34 @@ const SingleProduct = () => {
     const { productId } = useParams();
 
 
+
     const [product, setProduct] = useState({})
     useEffect(() => {
         fetch(`https://enigmatic-gorge-78786.herokuapp.com/products/${productId}`)
             .then(res => res.json())
             .then(data => setProduct(data))
     }, [])
+
+    const handleStock = (event) => {
+        event.preventDefault();
+        const restock = event.target.restock.value;
+        const updatedStock = { restock }
+        const url = `https://enigmatic-gorge-78786.herokuapp.com/products/${productId}`;
+
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedStock)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result)
+                alert('Product Added')
+                event.target.reset()
+            })
+    }
     return (
         <div>
             <Card style={{ width: '20rem' }}>
@@ -26,8 +48,8 @@ const SingleProduct = () => {
                     <p>Sold: {product.stock}</p>
                     <button className='btn btn-primary w-50 mb-2' >Delivered</button>
                     <form >
-                        <input className='w-50' type="number" name="number" id="" />
-                        <button className='btn btn-primary ms-2' type='submit'>Restock</button>
+                        <input className='w-50' type="number" name="restock" id="" />
+                        <button onClick={handleStock()} className='btn btn-primary ms-2' type='submit'>Restock</button>
                     </form>
                 </Card.Body>
             </Card>
